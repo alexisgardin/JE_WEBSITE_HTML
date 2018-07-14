d4bd7af7a46383a51e2a0e7ecf3bfbe1eab53bc1
Camille Jullien <jc502919@mjollnir.unice.fr>
2018-07-14 19:25:45 +0200

    mise en forme de l'accueil

--- js/scrollfire.js
+++ js/scrollfire.js
@@ -1,8 +1,8 @@
 var options = [
   {selector: '.projectFactsWrap', offset: 150, callback: function() {
     $('#number1').jQuerySimpleCounter({end: 12,duration: 3000});
-    $('#number2').jQuerySimpleCounter({end: 55,duration: 3000});
-    $('#number3').jQuerySimpleCounter({end: 359,duration: 2000});
+    $('#number2').jQuerySimpleCounter({end: 30,duration: 3000});
+    $('#number3').jQuerySimpleCounter({end: 20,duration: 3000});
   } },
 ];
 Materialize.scrollFire(options);
